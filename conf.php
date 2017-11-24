c46e2ff5ef194630f3dc81030827324151b8df1c
Simon Little <simon@glasselevatorstudios.com>
2017-11-24 10:50:18 +0000

    Remove keys

--- conf.php
+++ conf.php
@@ -4,6 +4,6 @@ if (!defined('WEATHERCHART')) {
 }
 $nameofapp = 'The Official Weather Chart';
 
-$apikey = '3a0373a19703c461dc9a200d54774ee6';
-$gmapskey = 'AIzaSyBib32YsJ-qXOWpyrfRjFrZvGm_YhZMKyE';
+$apikey = '';
+$gmapskey = '';
 ?>
